bd0c44602ae24e006fd12a719dae3f058f4c0a23
AlexSamak <2009kazan@gmail.com>
2020-09-18 15:09:50 +0400

    add Pickle dump load

---
+++ load_pickle_HTML.py
@@ -0,0 +1,66 @@
+# Read HTML from a file
+from bs4 import BeautifulSoup
+import pickle
+
+file_list = (
+    'soup1.pickle', 'soup2.pickle', 'soup3.pickle', 'soup4.pickle',
+    'soup5.pickle',)
+
+
+class Product:
+    name = str
+    price = str
+    in_stock = str
+    link = str
+
+    def __init__(self, name, price, in_stock, link):
+        self.name = name
+        self.price = price
+        self.in_stock = in_stock
+        self.link = link
+
+    def __repr__(self):
+        return str(self.__dict__)
+
+
+def get_soup(file_name):
+    """ Read the soup object from a file """
+    with open(file_name, "rb") as f:
+        soup_obj = pickle.load(f)
+    return soup_obj
+
+
+def add_objs(file_name: str, product_list: list, is_available: bool = True):
+    cnt = 0
+    soup = get_soup(file_name)
+
+    for element in soup.find_all('div',
+                                 class_="ms2_product cataloge__item_add"):
+        name = element.find('h3',
+                            class_="cataloge__title cataloge__title_add"). \
+            text.strip()
+        price = element.find('span', class_="c__price").text.strip()
+        in_stock = element.find('p', class_="in_store__line").text.strip()
+        link = ""
+
+        if in_stock.find('В наличии') >= 0 or not is_available:
+            product = Product(name, price, in_stock, link)
+            product_list.append(product)
+            cnt += 1
+    return cnt
+
+
+def main():
+    product_list = []
+    pos_cnt = 0
+
+    for file in file_list:
+        pos_cnt += add_objs(file, product_list, True)
+
+    for product in product_list:
+        print(product.__repr__())
+
+    print(f'\nВыведено {pos_cnt} позиций.')
+
+
+main()
---
+++ save_pickle_HTML.py
@@ -0,0 +1,76 @@
+import urllib.parse
+from urllib.request import urlopen, Request
+from bs4 import BeautifulSoup
+import pickle
+import sys
+
+base_link = "https://gazzap116.ru/search?query="
+sys.setrecursionlimit(20000)
+
+def get_page_amount(search_link: str):
+    """Find amount pages in HTML site """
+    html = urlopen(search_link)
+    soup = BeautifulSoup(html, 'html.parser')
+
+    el_pagination = soup.find('div',
+                              class_="mse2_pagination cataloge__pagination")
+    return len(el_pagination.find_all('a'))
+
+
+def get_html(find_link: str):
+    """ Return HTML data from find_link"""
+    r = Request(find_link)
+    # r.add_header('Cookie', 'sessionid=13cxrt4uytfc6ijvgeoflmb3u9jmjuhil; csrftoken=jdEKPN8iL62hdaq1hmMuID9DMALiiDIq')
+    r.add_header('authority', 'gazzap116.ru')
+    r.add_header('cache-control', 'max-age=0')
+    r.add_header('upgrade-insecure-requests', '1')
+    r.add_header('user-agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.106 Safari/537.36')
+    r.add_header('sec-fetch-dest', 'document')
+    r.add_header('accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9')
+    r.add_header('sec-fetch-site', 'same-origin')
+    r.add_header('sec-fetch-mode', 'navigate')
+    r.add_header('sec-fetch-user', '?1')
+    r.add_header('accept-language', 'en-US,en;q=0.9')
+    html = urlopen(r)
+    return html
+
+
+def get_page_link(find_link: str, page_num: int):
+    """ return html page link by page number"""
+    if page_num == 1:
+        link = find_link
+    else:
+        link = find_link + '&page=' + str(page_num)
+    return link
+
+
+def page_save(link: str, page_num: int):
+    """ Save Soup data to file by page number """
+    html = get_html(link)
+    soup = BeautifulSoup(html, "lxml")
+    # Save the soup object to a file
+    with open(f"soup{page_num}.pickle", "wb") as f:
+        pickle.dump(soup, f)
+
+
+def get_search_link():
+    """ return url+search request for first HTML pages"""
+    find_detail = input('Название детали: ').strip()
+    if find_detail == '':
+        return 'https://127.0.0.1'
+    return base_link + urllib.parse.quote(find_detail)
+
+
+def main():
+    """Download all pages with results by search bar from site www.gazzap116"""
+    search_link = get_search_link()
+    page_count = get_page_amount(search_link)
+    print(f'Количество страниц: {page_count}')
+
+    for page_item in range(1, page_count + 1):
+        def_link = get_page_link(search_link, page_item)
+        print(def_link)
+        page_save(def_link, page_item)
+
+
+main()
